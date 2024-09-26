6aaf56985e437bca304b3eea4918b7cdc98a73b8
Mathieu Poirier <mathieu.poirier@linaro.org>
2024-09-26 17:04:26 -0600

    feat(qemu-sbsa): Move DT related structures to their own header
    Move structure declaration related to the DT in their own header.  That
    way they can be reused by other files.
    
    No change in functionality.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

---
+++ plat/qemu/qemu_sbsa/include/sbsa_platform_dt.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024-2025, Linaro Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SBSA_PLATFORM_DT_H
+#define SBSA_PLATFORM_DT_H
+
+#include <platform_def.h>
+#include <stdint.h>
+
+typedef struct {
+	uint32_t nodeid;
+	uint32_t mpidr;
+} cpu_data;
+
+typedef struct {
+	uint32_t nodeid;
+	uint64_t addr_base;
+	uint64_t addr_size;
+} memory_data;
+
+/*
+ * sockets: the number of sockets on sbsa-ref platform.
+ * clusters: the number of clusters in one socket.
+ * cores: the number of cores in one cluster.
+ * threads: the number of threads in one core.
+ */
+typedef struct {
+	uint32_t sockets;
+	uint32_t clusters;
+	uint32_t cores;
+	uint32_t threads;
+} cpu_topology;
+
+struct qemu_platform_info {
+	uint32_t num_cpus;
+	uint32_t num_memnodes;
+	cpu_data cpu[PLATFORM_CORE_COUNT];
+	cpu_topology cpu_topo;
+	memory_data memory[PLAT_MAX_MEM_NODES];
+};
+
+#endif /* SBSA_PLATFORM_DT_H */
--- plat/qemu/qemu_sbsa/sbsa_sip_svc.c
+++ plat/qemu/qemu_sbsa/sbsa_sip_svc.c
@@ -10,6 +10,7 @@
 #include <common/fdt_wrappers.h>
 #include <common/runtime_svc.h>
 #include <libfdt.h>
+#include <sbsa_platform_dt.h>
 #include <smccc_helpers.h>
 
 /* default platform version is 0.0 */
@@ -36,39 +37,6 @@ static int platform_version_minor;
 #define SIP_SVC_GET_MEMORY_NODE SIP_FUNCTION_ID(301)
 
 static uint64_t gic_its_addr;
-
-typedef struct {
-	uint32_t nodeid;
-	uint32_t mpidr;
-} cpu_data;
-
-typedef struct{
-	uint32_t nodeid;
-	uint64_t addr_base;
-	uint64_t addr_size;
-} memory_data;
-
-/*
- * sockets: the number of sockets on sbsa-ref platform.
- * clusters: the number of clusters in one socket.
- * cores: the number of cores in one cluster.
- * threads: the number of threads in one core.
- */
-typedef struct {
-	uint32_t sockets;
-	uint32_t clusters;
-	uint32_t cores;
-	uint32_t threads;
-} cpu_topology;
-
-struct qemu_platform_info {
-	uint32_t num_cpus;
-	uint32_t num_memnodes;
-	cpu_data cpu[PLATFORM_CORE_COUNT];
-	cpu_topology cpu_topo;
-	memory_data memory[PLAT_MAX_MEM_NODES];
-};
-
 static struct qemu_platform_info dynamic_platform_info;
 
 void sbsa_set_gic_bases(const uintptr_t gicd_base, const uintptr_t gicr_base);
