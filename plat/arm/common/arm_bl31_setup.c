173bbd43b33435ed691690306c762093eec29675
Boyan Karatotev <boyan.karatotev@arm.com>
2026-02-03 14:04:19 +0000

    refactor(arm): set the transfer list up earlier
    It is needed to break the cyclic dependency between platform code and
    GIC code. This allows GIC code to use FCONF without depending on the
    rest of the platform setup.
    
    Change-Id: Idc704ece9c2292f23620448a0c87073114df87b0
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>

--- plat/arm/common/arm_bl31_setup.c
+++ plat/arm/common/arm_bl31_setup.c
@@ -385,37 +385,6 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
  ******************************************************************************/
 void arm_bl31_platform_setup(void)
 {
-	struct transfer_list_entry *te __unused;
-
-#if TRANSFER_LIST && !RESET_TO_BL31
-	ns_tl = transfer_list_init((void *)FW_NS_HANDOFF_BASE,
-				   PLAT_ARM_FW_HANDOFF_SIZE);
-	if (ns_tl == NULL) {
-		ERROR("Non-secure transfer list initialisation failed!\n");
-		panic();
-	}
-	/* BL31 may modify the HW_CONFIG so defer copying it until later. */
-	te = transfer_list_find(secure_tl, TL_TAG_FDT);
-	assert(te != NULL);
-
-	/* Populate HW_CONFIG device tree from transfer list entry */
-	fconf_populate("HW_CONFIG", (uintptr_t)transfer_list_entry_data(te));
-
-	te = transfer_list_add(ns_tl, TL_TAG_FDT, te->data_size,
-			       transfer_list_entry_data(te));
-	assert(te != NULL);
-
-	te = transfer_list_find(secure_tl, TL_TAG_TPM_EVLOG);
-	if (te != NULL) {
-		te = transfer_list_add(ns_tl, TL_TAG_TPM_EVLOG, te->data_size,
-				  transfer_list_entry_data(te));
-		if (te == NULL) {
-			ERROR("Failed to load event log in Non-Secure transfer list\n");
-			panic();
-		}
-	}
-#endif /* TRANSFER_LIST && !RESET_TO_BL31 */
-
 #if RESET_TO_BL31
 	/*
 	 * Do initial security configuration to allow DRAM/device access
@@ -590,6 +559,38 @@ void __init arm_bl31_plat_arch_setup(void)
 #endif /* ENABLE_RME */
 
 	arm_setup_romlib();
+
+	struct transfer_list_entry *te __unused;
+
+#if TRANSFER_LIST && !RESET_TO_BL31
+	ns_tl = transfer_list_init((void *)FW_NS_HANDOFF_BASE,
+				   PLAT_ARM_FW_HANDOFF_SIZE);
+	if (ns_tl == NULL) {
+		ERROR("Non-secure transfer list initialisation failed!\n");
+		panic();
+	}
+	/* BL31 may modify the HW_CONFIG so defer copying it until later. */
+	te = transfer_list_find(secure_tl, TL_TAG_FDT);
+	assert(te != NULL);
+
+	/* Populate HW_CONFIG device tree from transfer list entry */
+	fconf_populate("HW_CONFIG", (uintptr_t)transfer_list_entry_data(te));
+
+	te = transfer_list_add(ns_tl, TL_TAG_FDT, te->data_size,
+			       transfer_list_entry_data(te));
+	assert(te != NULL);
+
+	te = transfer_list_find(secure_tl, TL_TAG_TPM_EVLOG);
+	if (te != NULL) {
+		te = transfer_list_add(ns_tl, TL_TAG_TPM_EVLOG, te->data_size,
+				  transfer_list_entry_data(te));
+		if (te == NULL) {
+			ERROR("Failed to load event log in Non-Secure transfer list\n");
+			panic();
+		}
+	}
+#endif /* TRANSFER_LIST && !RESET_TO_BL31 */
+
 }
 
 void __init bl31_plat_arch_setup(void)
