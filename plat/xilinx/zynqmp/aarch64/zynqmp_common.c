gwshan/arm-trusted-firmware

commit 8f9ba3f344545740fc44e90fb8322c7728ae94ec

feat(zynqmp): add SMCCC_ARCH_SOC_ID support
Add support for calling SMCCC_ARCH_SOC_ID which is used by Linux soc_id
driver for printing information about manufacturer and also chip version
and silicon ID code. SOC revision is directly mapped to chip ID code.
And SOC version is composed from manufacturer ID based on JEP-106 with
chip_id which contains bits mapped to CPU register 0xffca0044 platform
bits which differentiate between silicon, qemu and other emulated
platforms.

Function description is available at
docs/getting_started/porting-guide.rst.

Change-Id: I1f19e1973593897e71b39244dbdbceb6bd0e8a07
Signed-off-by: Michal Simek <michal.simek@amd.com>

plat/xilinx/zynqmp/aarch64/zynqmp_common.c
@@ -11,10 +11,13 @@
 #include <common/debug.h>
 #include <drivers/generic_delay_timer.h>
 #include <lib/mmio.h>
+#include <lib/smccc.h>
 #include <lib/xlat_tables/xlat_tables.h>
 #include <plat_ipi.h>
 #include <plat_private.h>
+#include <plat_startup.h>
 #include <plat/common/platform.h>
+#include <services/arm_arch_svc.h>
 
 #include "pm_api_sys.h"
 
@@ -309,6 +312,31 @@ static char *zynqmp_print_silicon_idcode(void)
 	}
 	VERBOSE("IDCODE 0x%x\n", id);
 	return zynqmp_get_silicon_idcode_name();
+}
+
+int32_t plat_is_smccc_feature_available(u_register_t fid)
+{
+	switch (fid) {
+	case SMCCC_ARCH_SOC_ID:
+		return SMC_ARCH_CALL_SUCCESS;
+	default:
+		return SMC_ARCH_CALL_NOT_SUPPORTED;
+	}
+
+	return SMC_ARCH_CALL_NOT_SUPPORTED;
+}
+
+int32_t plat_get_soc_version(void)
+{
+	uint32_t chip_id = zynqmp_get_silicon_ver();
+	uint32_t manfid = SOC_ID_SET_JEP_106(JEDEC_XILINX_BKID, JEDEC_XILINX_MFID);
+
+	return (int32_t)(manfid | (chip_id & 0xFFFF));
+}
+
+int32_t plat_get_soc_revision(void)
+{
+	return mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_IDCODE_OFFSET);
 }
 
 static uint32_t zynqmp_get_ps_ver(void)

plat/xilinx/common/include/plat_startup.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -37,5 +38,9 @@ struct xfsbl_atf_handoff_params {
 enum fsbl_handoff fsbl_atf_handover(entry_point_info_t *bl32,
 					entry_point_info_t *bl33,
 					uint64_t atf_handoff_addr);
+
+/* JEDEC Standard Manufacturer's Identification Code and Bank ID JEP106 */
+#define JEDEC_XILINX_MFID	U(0x49)
+#define JEDEC_XILINX_BKID	U(0)
 
 #endif /* PLAT_STARTUP_H */

commit 8f218dd07a375d49c6187c0b1be6bee4673187f6

fix(zynqmp): correct SOC ID version and revision
plat_get_soc_version() uses CSU_VERSION[15:12] which returns zero
for all real silicon variants. Fix to use CSU_IDCODE[27:12], the
stable JTAG part-number field.

plat_get_soc_revision() returns the full IDCODE register including
manufacturer bits. Fix to use CSU_VERSION[30:0] which carries
PS_VER and RTL_VER that change across silicon steppings.

Also add a cached CSU_VERSION accessor to avoid repeated register
reads.

Fixes: 8f9ba3f34454 ("feat(zynqmp): add SMCCC_ARCH_SOC_ID support")
Change-Id: Id2e56a560e6d17a6c684c4ff8cb59b23dee92e56
Signed-off-by: Sai Varun Venkatapuram <saivarun.venkatapuram@amd.com>

plat/xilinx/zynqmp/aarch64/zynqmp_common.c
@@ -41,16 +41,23 @@ const mmap_region_t *plat_get_mmap(void)
 	return plat_zynqmp_mmap;
 }
 
-static uint32_t zynqmp_get_silicon_ver(void)
+static uint32_t zynqmp_get_csu_version(void)
 {
 	static uint32_t ver;
 
 	if (ver == 0U) {
-		ver = mmio_read_32(ZYNQMP_CSU_BASEADDR +
-				   ZYNQMP_CSU_VERSION_OFFSET);
-		ver &= ZYNQMP_SILICON_VER_MASK;
-		ver >>= ZYNQMP_SILICON_VER_SHIFT;
+		ver = mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_VERSION_OFFSET);
 	}
+
+	return ver;
+}
+
+static uint32_t zynqmp_get_silicon_ver(void)
+{
+	uint32_t ver = zynqmp_get_csu_version();
+
+	ver &= ZYNQMP_SILICON_VER_MASK;
+	ver >>= ZYNQMP_SILICON_VER_SHIFT;
 
 	return ver;
 }
@@ -287,9 +294,7 @@ static char *zynqmp_get_silicon_idcode_name(void)
 
 static unsigned int zynqmp_get_rtl_ver(void)
 {
-	uint32_t ver;
-
-	ver = mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_VERSION_OFFSET);
+	uint32_t ver = zynqmp_get_csu_version();
 	ver &= ZYNQMP_RTL_VER_MASK;
 	ver >>= ZYNQMP_RTL_VER_SHIFT;
 
@@ -332,21 +337,35 @@ int32_t plat_is_smccc_feature_available(u_register_t fid)
 
 int32_t plat_get_soc_version(void)
 {
-	uint32_t chip_id = zynqmp_get_silicon_ver();
+	uint32_t idcode = mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_IDCODE_OFFSET);
 	uint32_t manfid = SOC_ID_SET_JEP_106(JEDEC_XILINX_BKID, JEDEC_XILINX_MFID);
-	uint32_t result = (manfid | (chip_id & 0xFFFFU));
+	/*
+	 * IDCODE[27:12] = FAMILY(7) + SUB_FAMILY(2) + DEVICE_CODE(4) + SVD(3)
+	 * = stable 16-bit SoC part-number, does not change across steppings.
+	 */
+	uint32_t soc_id = (idcode >> ZYNQMP_CSU_IDCODE_SVD_SHIFT) &
+			  SOC_ID_IMPL_DEF_MASK;
+	uint32_t result = manfid | soc_id;
 
 	return (int32_t)result;
 }
 
 int32_t plat_get_soc_revision(void)
 {
-	return (int32_t)mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_IDCODE_OFFSET);
+	/*
+	 * CSU_VERSION[30:0] carries PS_VER[3:0], RTL_VER[11:4], and
+	 * SILICON_VER[15:12]. PS_VER and RTL_VER change across silicon
+	 * steppings, satisfying the SMCCC revision stability requirement.
+	 */
+	uint32_t ver = zynqmp_get_csu_version();
+	uint32_t result = ver & SOC_ID_REV_MASK;
+
+	return (int32_t)result;
 }
 
 static uint32_t zynqmp_get_ps_ver(void)
 {
-	uint32_t ver = mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_VERSION_OFFSET);
+	uint32_t ver = zynqmp_get_csu_version();
 
 	ver &= ZYNQMP_PS_VER_MASK;
 	ver >>= ZYNQMP_PS_VER_SHIFT;